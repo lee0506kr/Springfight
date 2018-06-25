97d2bd27b1d76dc787195fbf92e3c7a5a8c483fc
jun <lee0506kr@nate.com>
2018-06-25 16:45:07 +0900

    mybatis

---
+++ Day0625_Mybatis3_Spring/src/dao/DaoTest.java
@@ -0,0 +1,19 @@
+package dao;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class DaoTest {
+	public static void main(String[] args) {
+		ApplicationContext context
+		 = new GenericXmlApplicationContext("dao/applicationContext.xml");
+		
+		
+		StudentDao dao = context.getBean("studentDao",StudentDao.class);
+		System.out.println(dao.selectAll());
+		
+		
+		
+	}
+
+}
